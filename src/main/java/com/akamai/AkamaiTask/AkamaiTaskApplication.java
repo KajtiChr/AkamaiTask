18ba19f819ce9e7d140f15566e5857b3595f0bac
KajtiChr <kajchrzanowski@gmail.com>
2023-02-13 15:34:35 +0100

    Generated project

---
+++ src/main/java/com/akamai/AkamaiTask/AkamaiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.akamai.AkamaiTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkamaiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkamaiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akamai/AkamaiTask/AkamaiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akamai.AkamaiTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkamaiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
