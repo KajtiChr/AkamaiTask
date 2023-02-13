bf0c91d73bd38dfcb6edd51aabb474f6baa1ca8c
KajtiChr <kajchrzanowski@gmail.com>
2023-02-13 16:02:19 +0100

    Add DB setup

---
+++ src/main/resources/DB-Setup.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE  IF NOT EXISTS `Task`;
+USE `Task`;
+
+CREATE TABLE `SocialNetworkPost`(
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `post_date` DATETIME,
+    `author` varchar(40) DEFAULT NULL,
+    `content` text DEFAULT NULL,
+    `view_count` BIT(25)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
